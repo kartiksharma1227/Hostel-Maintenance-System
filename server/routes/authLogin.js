d3eb898b40614e1f1db1a34190c3191374c32a07
Ishita Agrawal <ishitaagrawal2020@gmail.com>
2025-06-18 23:32:43 +0530

    Login working fully

--- server/routes/authLogin.js
+++ server/routes/authLogin.js
@@ -2,9 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
-const { studentLogin } = require('../controllers/authController.js');
-
+// const { studentLogin,adminLogin } = require('../controllers/authController.js');
+const {login}= require('../controllers/authController.js');
 // POST /api/student/login
-router.post('/student/login', studentLogin);
+// router.post('/student/login', studentLogin);
+// POST /api/admin/login
+// router.post('/admin/login', adminLogin);
+router.post('/login', login); // Unified login route for both students and admins
 
 module.exports = router;
